ccaf3742e89ec678d683b0089dcb00905a222779
Nahum Sá <nahumsaa@gmail.com>
2023-09-30 14:03:41 -0300

    fix import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image'
-import HeroSection from "./components/herosection.tsx"
+import HeroSection from "./components/herosection"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-12">
-      <div class="container">
+      <div className="container">
         <HeroSection />
       </div>
     </main>
