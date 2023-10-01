9b38472af31b4eb12e70bf603c45c82263b0667b
Nahum Sá <nahumsaa@gmail.com>
2023-09-30 22:39:40 -0300

    finish up header section

--- src/app/components/herosection.tsx
+++ src/app/components/herosection.tsx
@@ -1,20 +1,29 @@
 import React from "react";
-import Image from "next/image";
+import Link from 'next/link';
+import {FaLinkedin, FaGithub} from 'react-icons/fa6';
 
 const HeroSection = () => {
   return (
     <section>
       <div className="grid grid-cols-1 lg:grid-cols-12">
         <div className="col-span-7 place-self-center">
-          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
+          <h1 className="text-emerald-200 mb-4 mt-4 text-4xl lg:text-6xl font-extrabold">
             Nahum Sá
           </h1>
-          <p className="text-white-100 text-lg mb-4 lg:text-xl">
-            Data Scientist and Engineer experienced in creating solutions that have scalability and consistency in mind.
+          <p className="text-white-100 text-base sm:text-lg mb-4 lg:text-xl">
+            Data and Machine Learning Engineer experienced in creating solutions that have scalability and consistency in mind.
           </p>
-          <div>
-            <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black">Github</button>
-            <button className="px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3">Linkedin</button>
+          <div className="place-self-center">
+            <a target="_blank" href="https://www.linkedin.com/in/nahumsa/">
+              <button className="h-12 px-6 m-2 text-lg text-white-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-emerald-300">
+                <FaLinkedin />
+              </button>
+            </a>
+            <a target="_blank" href="https://github.com/nahumsa">
+              <button className="h-12 px-6 m-2 text-lg text-white-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-emerald-300">
+                <FaGithub />
+              </button>
+            </a>
           </div>
           </div>
         <div className="col-span-5 place-self-center mt-4 lg:mt-0">
