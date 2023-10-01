d79f07bede12347542c6ff2f951eb9e4853016be
Nahum Sá <nahumsaa@gmail.com>
2023-09-30 22:49:21 -0300

    add favicon and fix name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,10 @@ import HeroSection from "./components/herosection"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-12">
+      <link rel="shortcut icon" href="/images/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
       <div className="container">
         <HeroSection />
       </div>
